f560db16874115af597e344e135f12b59213c397
rahathossain3 <rahathossain210@gmail.com>
2022-03-30 11:57:29 +0600

    first commit

---
+++ src/components/Friends/Friends.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Friends = () => {
+    return (
+        <div>
+            <h2>Hello, Friends How are You...</h2>
+            <p><small>Parle kicu taka dar de</small></p>
+        </div>
+    );
+};
+
+export default Friends;
