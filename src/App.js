834979ad17a455e75cce61ccfb516e7b884af093
rahathossain3 <rahathossain210@gmail.com>
2022-03-30 12:38:31 +0600

    react route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home/Home';
+import Friends from './components/Friends/Friends';
+import About from './components/About/About';
 
 function App() {
   return (
     <div className="App">
-
+      <Home></Home>
+      <Friends></Friends>
+      <About></About>
     </div>
   );
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-
+            <h2>This  is Home </h2>
+            <p>Place for goom</p>
         </div>
     );
 };
